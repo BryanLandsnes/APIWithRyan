47e338ea7c0d57f950e4c1fd00dbf592eb29666c
Bryan Landsnes <blandsnes6236@stu.d214.org>
2023-11-21 10:11:20 -0600

    Initial Commit

---
+++ APIWithRyan/APIWithRyanApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIWithRyanApp.swift
+//  APIWithRyan
+//
+//  Created by Bryan C. Landsnes on 11/21/23.
+//
+
+import SwiftUI
+
+@main
+struct APIWithRyanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
